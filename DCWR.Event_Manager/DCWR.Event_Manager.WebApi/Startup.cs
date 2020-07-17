1b5c0ede0203a67ee1f66b2d095320c74814e276
sercan.aksoy@shijigroup.com <sercan.aksoy@shijigroup.com>
2020-07-17 15:22:08 +0200

    bootstrap

--- DCWR.Event_Manager/DCWR.Event_Manager.WebApi/Startup.cs
+++ DCWR.Event_Manager/DCWR.Event_Manager.WebApi/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DCWR.Event_Manager.WebApi
 {
@@ -27,8 +22,13 @@ namespace DCWR.Event_Manager.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var eventManagerBootstrap = new Bootstrap(services);
+            eventManagerBootstrap.RunDiRegistrations();
             services.AddDbContext<EventManagerDbContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAuthentication()
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => Configuration.Bind("JwtSettings", options))
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options => Configuration.Bind("CookieSettings", options));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
