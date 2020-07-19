6ed26a31232c20c70d390ab981d0cca08966ef25
sercan.aksoy@shijigroup.com <sercan.aksoy@shijigroup.com>
2020-07-19 10:22:21 +0200

    transfer to webapp

---
+++ DCWR.Event_Manager/DCWR.Event_Manager.WebApp.React/Controllers/Users/Contracts/AuthenticateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DCWR.Event_Manager.WebApp.React.Controllers.Users.Contracts
+{
+    public class AuthenticateRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DCWR.Event_Manager/DCWR.Event_Manager.WebApp.React/Controllers/Users/Services/AuthenticationService.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using DCWR.Event_Manager.Contracts.Users.Entities;
+using DCWR.Event_Manager.Contracts.Users.Queries;
+using DCWR.Event_Manager.Infrastructure;
+using DCWR.Event_Manager.WebApp.React.Controllers.Users.Contracts;
+
+namespace DCWR.Event_Manager.WebApp.React.Controllers.Users.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
+    }
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly ICommandQueryDispatcher dispatcher;
+        private readonly IJwtTokenGenerator tokenGenerator;
+
+        public AuthenticationService(
+            ICommandQueryDispatcher dispatcher,
+            IJwtTokenGenerator tokenGenerator)
+        {
+            this.dispatcher = dispatcher;
+            this.tokenGenerator = tokenGenerator;
+        }
+
+        public async Task<AuthenticateResponse> Authenticate(AuthenticateRequest request)
+        {
+            var user = await dispatcher
+                .DispatchAsync<UserData, AuthenticateUser>(new AuthenticateUser(request.Username, request.Password));
+            var token = tokenGenerator.GenerateJwtToken(user.Id);
+            return new AuthenticateResponse(
+                id: user.Id,
+                token: token
+            );
+        }
+    }
+}
