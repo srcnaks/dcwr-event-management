ee2af40f06250c45a20b2095b462b57652293beb
sercan.aksoy@shijigroup.com <sercan.aksoy@shijigroup.com>
2020-07-19 09:50:43 +0200

    react app added

---
+++ DCWR.Event_Manager/DCWR.Event_ManagerWebApp.React/Services/JwtTokenGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DCWR.Event_ManagerWebApp.React.Services
+{
+    public class JwtTokenGenerator
+    {
+    }
+}
